d255ac9f5db1c6f4ef2eb19151a497cccac946a0
rushShahay <rushhaank.sahay@gmail.com>
2024-11-01 19:42:03 -0500

    AdvantageScope visualizations
    Code now adds the pose of the bot into advantage scope using networktables for visualizations. Along with this the code now sets values to smartdashboard so they can be viewed in AdvantageScope.

--- src/main/java/frc/robot/subsystems/AprilTagStats.java
+++ src/main/java/frc/robot/subsystems/AprilTagStats.java
@@ -1,24 +1,22 @@
 package frc.robot.subsystems;
 
-import java.util.List;
-
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants;
 import frc.robot.Constants.cameraRotationConstants;
 import frc.robot.Constants.cameraTranslationConstants;
 
@@ -31,11 +29,19 @@ public class AprilTagStats extends SubsystemBase{
     
     //Positonal object representation of the camera on the robot
     Transform3d robotToCam = new Transform3d(new Translation3d(cameraTranslationConstants.tX, cameraTranslationConstants.tY, cameraTranslationConstants.tZ), new Rotation3d(cameraRotationConstants.rRoll, cameraRotationConstants.rPitch, cameraRotationConstants.rYaw));
-    
+    PhotonPoseEstimator m_photonPoseEstimator;
+
     double yaw, pitch, area;
     int id;
+    StructPublisher<Pose3d> arrPub;
 
     public AprilTagStats() {
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    NetworkTable table = inst.getTable("datatable");
+    arrPub = table.getStructTopic("Pose", Pose3d.struct).publish();
+    m_photonPoseEstimator = new PhotonPoseEstimator(layout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, arduCam, new Transform3d());
+    m_photonPoseEstimator.setReferencePose(new Pose3d(12.5, 5.5, 0, new Rotation3d()));
+
         //construction of a new pose estimator (not tested)
         //PhotonPoseEstimator estimator = new PhotonPoseEstimator(layout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, arduCam, robotToCam);
     }
@@ -63,10 +69,12 @@ public class AprilTagStats extends SubsystemBase{
 
                 //gets the transformation that maps the space between the camera and the tag 
                 Transform3d transform = target.getBestCameraToTarget();
-
+                var update = m_photonPoseEstimator.update();
                 //if an april tag is spotted, calculates and returns the 3d pose of the robot (robot relative to field including translation and rotation)
                 if (layout.getTagPose(target.getFiducialId()).isPresent()) {
                     Pose3d relativePose = relativeRobot(transform, target, robotToCam, layout);
+                    if (Math.abs(relativePose.getZ()) < 1) m_photonPoseEstimator.setReferencePose(update.get().estimatedPose);
+                    arrPub.set(update.get().estimatedPose);
                 }
             }
             System.out.println("connected");
@@ -79,10 +87,12 @@ public class AprilTagStats extends SubsystemBase{
 
     public void updateView(double yaw, double pitch, double area, double id) {
         //pushes yaw, pitch, area, and id to ShuffleBoard (Meant for testing if values are being passed to variables)
-        Shuffleboard.getTab("Vision").add("yaw", yaw);
-        Shuffleboard.getTab("Vision").add("pitch", pitch);
-        Shuffleboard.getTab("Vision").add("area", area);
-        Shuffleboard.getTab("Vision").add("id", id);
+        SmartDashboard.putNumber("yaw", yaw);
+      SmartDashboard.putNumber("pitch",pitch);
+      SmartDashboard.putNumber("area", area);
+      SmartDashboard.putNumber("id",id);
+
+
     }
 
     public Pose3d relativeRobot(Transform3d transform, PhotonTrackedTarget target, Transform3d robotToCamera, AprilTagFieldLayout layout) {
